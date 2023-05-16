98147022322d810ca13b6c46a8f9062dd95c4c43
ishibashi633 <ishibashi.ryosuke633@mail.kyutech.jp>
2023-05-16 17:35:07 +0900

    もろもろテスト用のやつ

---
+++ test.py
@@ -0,0 +1,33 @@
+# なんかいろいろテスト用
+
+import numpy as np
+from tqdm import tqdm
+
+
+if __name__ == "__main__":
+    print("main")
+
+    N_x = 100
+    input_musk  = [0, 0, 1, 1, 0, 1, 0, 0, 1, 1] # 5
+    # input_musk = None
+    output_musk = [1, 1, 1, 0, 1, 0, 0, 1, 0, 1] # 6
+
+    x = np.array([i+10 for i in range(10)])
+    x_in = np.array([i+20 for i in range(10)])
+
+    # print([x for x in output_musk if x != 0])
+
+    input_num = N_x if input_musk == None else np.sum(input_musk)
+    output_num = N_x if output_musk == None else len([x for x in output_musk if x != 0])
+
+
+    print(input_num, output_num)
+
+
+    x_prime = x[np.nonzero(input_musk)]
+    print(x_prime)
+
+    x_in *= input_musk
+    print(x_in)
+
+
