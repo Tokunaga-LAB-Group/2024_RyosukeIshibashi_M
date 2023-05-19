1dedde1e8460e6efb109b0938b92d915485b85b9
ishibashi633 <ishibashi.ryosuke633@mail.kyutech.jp>
2023-05-19 19:52:16 +0900

    import追加とその他もろもろ

--- test.py
+++ test.py
@@ -1,7 +1,22 @@
 # なんかいろいろテスト用
 
+import sys
+import pathlib
+# 実行ファイルのあるディレクトリの絶対パスを取得
+current_dir = pathlib.Path(__file__).resolve().parent
+# モジュールのあるパスを追加
+sys.path.append( str(current_dir) + "\..\\" )
+import argparse
+import os
+import time
+import csv
 import numpy as np
+import matplotlib.pyplot as plt
 from tqdm import tqdm
+from models.model import ESN, Tikhonov
+from orglib import make_dataset as md
+from orglib import stegano as stg
+from orglib import read_csv as rc
 
 
 if __name__ == "__main__":
@@ -31,5 +46,38 @@ if __name__ == "__main__":
     # print(x_in)
 
     outputMusk = np.array([1 if i<32 else 0 for i in range(N_x)]) # 出力ノード数
-    print(outputMusk.shape, outputMusk, outputMusk.sum())
+    # print(outputMusk.shape, outputMusk, outputMusk.sum())
+
+    periodValue = [0, 1, 0]
+    periodDura = [3, 5, 2]
+    # print([(periodDura[i], periodValue[i]) for i in range(len(periodValue))])
+    # trainPeriod = np.concatenate(np.full((periodDura[i], periodValue[i]) for i in range(len(periodValue))))
+    # print(trainPeriod)
+
+    csv_filepath = "./input/"
+    csv_filename = [
+        "data_10-5_N2_300.csv", 
+        "data_10-6_N2_300.csv", 
+        "data_10-7_N2_300.csv", 
+        "data_10-8_N2_300.csv", 
+        "data_10-9_N2_300.csv", 
+        "data_0_N2_300.csv"
+        ]
+    csvFname = []
+    for fname in csv_filename:
+        csvFname.append(csv_filepath + fname)
+    TEST = ["10-5", "10-6"]
+
+    
+    csvData, inputData, csvDatasMean, csvDatasStd = rc.readCsvAll(csvFname, 300)
+    testData = []
+    if TEST is None: # テスト対象未指定なら全部でやる
+        TEST = ["10-5", "10-6", "10-7", "10-8", "10-9", "0"]
+    print(csvDatasMean["10-5"].reshape(-1, 1).shape)
+    for test in TEST:
+        # testData = np.concatenate((testData, csvDatasMean[test].reshape(-1, 1)), axis=1)
+        testData.extend(csvDatasMean[test].reshape(-1, 1))
+    
+    testData = np.array(testData)
+    print(testData.shape)
 
