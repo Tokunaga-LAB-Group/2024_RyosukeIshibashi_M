f4cbd5f949ac8ddfc0826a5af7480016741fb04b
ishibashi633 <ishibashi.ryosuke633@mail.kyutech.jp>
2024-07-24 17:01:03 +0900

    マルチリザバーを使うように

--- test/memory_capacity.py
+++ test/memory_capacity.py
@@ -30,13 +30,13 @@ cp.random.seed(seed=0)
 if __name__ == '__main__':
 
     # 時系列入力データ生成
-    T = 500  # 長さ
+    T = 2000  # 長さ
     u = cp.random.rand(T,1)-0.5  # 区間[-0.5, 0.5]の乱数系列
 
     # 時系列出力データ生成
-    delay = cp.arange(50)  # 遅延長
+    delay = cp.arange(40)  # 遅延長
     d = cp.empty((T, len(delay)))
-    for k in range(len(delay)):
+    for k in tqdm(range(len(delay))):
         for t in range(T):
             d[t, k] = u[t-delay[k]]  # 遅延系列
 
@@ -49,34 +49,34 @@ if __name__ == '__main__':
 
 
     #### layer
-    nodeNum = 100
+    nodeNum = 600
 
     # Input
-    inputLayer = InputLayer(train_U.shape[1], nodeNum, inputScale=1)
+    inputLayer = InputLayer(train_U.shape[1], 128, inputScale=1)
 
 
     # model = ESN(train_U.shape[1], train_D.shape[1], N_x, density=0.05, 
     #             input_scale=1.0, rho=0.8, leak_rate=1.0)
 
     # Reservoir
-    reservoirLayer = ReservoirLayer(nodeNum, nodeNum, nodeNum, 0.2, 0.8, cp.tanh, 1.0)
+    # reservoirLayer = ReservoirLayer(128, 256, nodeNum, 0.24, 0.8, cp.tanh, 1.0)
 
-    # resInput1 = InputLayer(16, 32, inputScale=1, seed=11)
-    # resRes1 = ReservoirLayer(32, 48, nodeNum, 0.2, 0.95, cp.tanh, 0.22, seed=101)
+    resInput1 = InputLayer(inputLayer.outputDimention, 128, inputScale=1, seed=11)
+    resRes1 = ReservoirLayer(resInput1.outputDimention, 128, nodeNum, 0.24, 0.8, cp.tanh, 1.0, seed=101)
 
-    # resInput2 = InputLayer(16, 16, inputScale=1, seed=12)
-    # resRes2 = ReservoirLayer(16, 16, nodeNum, 0.3, 0.95, cp.tanh, 0.22, seed=102)
+    resInput2 = InputLayer(inputLayer.outputDimention, 128, inputScale=1, seed=12)
+    resRes2 = ReservoirLayer(resInput2.outputDimention, 128, nodeNum, 0.24, 0.8, cp.tanh, 1.0, seed=102)
 
     # resInput3 = InputLayer(16, 16, inputScale=1, seed=13)
     # resRes3 = ReservoirLayer(16, 64, nodeNum, 0.3, 0.95, cp.tanh, 0.22, seed=103)
 
-    # reservoirLayer = ParallelReservoirLayer(16, 64, [(resInput1, resRes1), (resInput2, resRes2)])
+    reservoirLayer = ParallelReservoirLayer(inputLayer.outputDimention, 256, [(resInput1, resRes1), (resInput2, resRes2)])
     # reservoirLayer = SerialReservoirLayer(16, 64, [resRes2, resRes3], 1)
     # reservoirLayer = BothReservoirLayer(16, 64, [(resInput1, resRes1), (resInput2, resRes2)], 1)
 
 
     # Output
-    outputLayer = OutputLayer(nodeNum, train_D.shape[1])
+    outputLayer = OutputLayer(256, train_D.shape[1])
 
 
 
@@ -109,11 +109,11 @@ if __name__ == '__main__':
     plt.plot(delay.get(), DC, color='k', marker='o')
     plt.ylim([0, 1.1])
     # plt.xticks([0, 5, 10, 15, 20])
-    plt.title('MC ~ %3.2lf' % MC[0], x=0.8, y=0.9)
+    plt.title('MC ~ %3.2lf' % MC, x=0.8, y=0.9)
     plt.xlabel('Delay k')
     plt.ylabel('Determination coefficient')
 
     # 生成するファイル名
-    fname = "../output/20240716/memory_capacity_03.png"
+    fname = "../output/20240716/memory_capacity_22.png"
     # 保存
     plt.savefig(fname, bbox_inches="tight", pad_inches=0.05, dpi=400)
