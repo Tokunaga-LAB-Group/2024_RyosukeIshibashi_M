6820c41c68f5ba2dd9e91c88dd69bad46b43f788
ishibashi633 <ishibashi.ryosuke633@mail.kyutech.jp>
2024-09-18 15:50:26 +0900

    csvからjsonに変換するやつを調整

--- orglib/csv_to_json.py
+++ orglib/csv_to_json.py
@@ -3,55 +3,79 @@
 import csv
 import json
 import numpy as np
+import os
+import pandas as pd
 
 # ファイルパスとファイル名(複数可)
-FILEPATH="../input/"
-FILENAME=["data_10-5_N2_300.csv", "data_10-6_N2_300.csv", "data_10-7_N2_300.csv" ,"data_10-8_N2_300.csv", "data_10-9_N2_300.csv", "data_0_N2_300.csv"]
+FILEDIR="/home/ishibashi/Reservoir_ESN/input/Original_Data_csv/Figure5/Figure5AB" # とりあえず今はこれだけ
+# FILENAME=["data_10-5_N2_300.csv", "data_10-6_N2_300.csv", "data_10-7_N2_300.csv" ,"data_10-8_N2_300.csv", "data_10-9_N2_300.csv", "data_0_N2_300.csv"]
 
 if __name__ == "__main__":
     print("start")
     
 
-    ID = 1
-    pattern = "p1"
-    stim = "-5"
+    fileNameList = np.asarray(os.listdir(FILEDIR))
+    # print(fileNameList)
 
-    dummy = [0, 1, 2, 3, 4, 5]
+    ID = 1
+    pattern = "p2"
+    stim = "-5"
+    type = "N2"
 
     dictData = {}
 
-    for fname in FILENAME:
-        filename = FILEPATH + fname
+    # for current_dir, sub_dirs, files_list in os.walk(FILEPATH): 
+    #     print("hoge")
+
+
+    for fname in fileNameList:
+        filename = os.path.join(FILEDIR, fname)
+        print(filename)
 
         # ファイル開く
         F = open(filename, "r")
 
         # 濃度設定
-        stim = "-" + filename.split("_")[1].split("-")[-1]
+        stim = "-" + filename.split(".")[0][-1]
         # print(stim)
 
-        #ファイルからデータを読み込み
-        rows = csv.reader(F, quoting=csv.QUOTE_NONNUMERIC)
+        # type設定
+        type = filename.split("_")[4]
+        # print(type)
 
-        # for文で行を1つずつ取り出す
+        #ファイルからデータを読み込み
+        # rows = csv.reader(F, quoting=csv.QUOTE_NONNUMERIC)
+        df = pd.read_csv(F, header=None, index_col=None)
+
+        # # for文で行を1つずつ取り出す
+        # data = []
+        # for row in rows: 
+        #     # データ作成(辞書)
+        #     print(row)
+        #     dData = {"pattern":pattern, "stim":stim, "type":type, "data":row}
+        #     dictData[ID] = dData
+
+        #     ID += 1
+
+        # for文で列を1つずつ取り出す
         data = []
-        for row in rows: 
+        for col in df.columns:
+            colData = df[col]
+            # print(colData.values)
             # データ作成(辞書)
-            dData = {"pattern":pattern, "stim":stim, "data":row}
+            dData = {"pattern":pattern, "stim":stim, "type":type, "data":list(colData.values)}
             dictData[ID] = dData
 
             ID += 1
 
-
         # ファイル閉じる
         F.close()
 
-        print(filename)
 
 
     jsonData = json.dumps(dictData, ensure_ascii=False, indent=4)
     # print(jsonData)
 
     # jsonファイルに記録
-    with open(FILEPATH + "data_all.json", 'w') as f:
+    with open(os.path.join("/home/ishibashi/Reservoir_ESN/input", "data_unveiled.json"), 'w') as f:
         f.write(jsonData)
