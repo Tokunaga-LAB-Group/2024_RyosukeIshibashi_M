01a6da1fb20b86d577ec4e94fc78133ec293a3a1
ishibashi633 <ishibashi.ryosuke633@mail.kyutech.jp>
2025-02-21 14:19:11 +0900

    最適解を出力できるように

--- reproduction/visualization_optuna.py
+++ reproduction/visualization_optuna.py
@@ -20,7 +20,7 @@ if __name__ == "__main__" :
 
     # 結果のロード
     study = optuna.load_study(
-        study_name="reproduct_sample06", 
+        study_name="reproduct_sample07", 
         storage="mysql://ishibashi@127.0.0.1/ishibashi_optuna_01",
     )
 
@@ -32,18 +32,18 @@ if __name__ == "__main__" :
     # fig = plot_optimization_history(study)
     # fig = plot_parallel_coordinate(study)
     # fig = plot_param_importances(study)
-    # fig = plot_rank(study)
-    fig = plot_slice(study)
+    fig = plot_rank(study)
+    # fig = plot_slice(study)
     # fig = plot_timeline(study)
 
 
     # 表示のカスタマイズ
     # print(fig)
     # fig.set_ylim(0, 1)
-    plt.ylim(0.4, 2)
+    # plt.ylim(0.4, 2)
 
 
-    fname = "/home/ishibashi/Reservoir_ESN/output/20241101/" + "optuna_slice02_lim.png"
+    fname = "/home/ishibashi/Reservoir_ESN/output/20241101/" + "optuna_rank03.png"
 
     # 保存
     plt.savefig(fname, bbox_inches="tight", pad_inches=0.05, dpi=400)
@@ -51,4 +51,13 @@ if __name__ == "__main__" :
     plt.show()
     plt.close()
 
+    # 最適解出力
+    best_params = study.best_params
+    found_nodeNum = best_params["nodeNum"]
+    found_leaking_rate = best_params["leaking_rate"]
+    found_tikhonov_beta = best_params["tikhonov_beta"]
+    
+    best_value = study.best_value
+
+    print(f"Found nodeNum: {found_nodeNum}, Found leaking rate:{found_leaking_rate}, Found tikhonov beta : {found_tikhonov_beta}, value : {best_value}")
 
